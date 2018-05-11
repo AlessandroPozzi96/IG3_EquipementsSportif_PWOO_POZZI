9c8e35302b19a7629602b5e061af1af675c0f700
Gwynbleidd96 <alessandro.pozzi72@gmail.com>
2018-05-11 19:43:35 +0200

    Initial commit

---
+++ src/main/java/com/spring/henallux/IG3_EquipementsSportif_PWOO_POZZI/Ig3EquipementsSportifPwooPozziApplication.java
@@ -0,0 +1,12 @@
+package com.spring.henallux.IG3_EquipementsSportif_PWOO_POZZI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ig3EquipementsSportifPwooPozziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ig3EquipementsSportifPwooPozziApplication.class, args);
+	}
+}
